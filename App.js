d4ff2972e546157b532dd1b17b85ad7d35fa98c6
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2020-07-22 21:06:26 -0400

    Add minefield component with fixes

--- App.js
+++ App.js
@@ -6,29 +6,41 @@ import {
   Text,
 } from 'react-native';
 import params from './src/params'
-import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
   render() {
     return (
       <>
         <SafeAreaView style={styles.container}>
-          <Text style={styles.welcome}>Iniciando o Mines !!!</Text>
+          <Text style={styles.welcome}>Mines!</Text>
           <Text style={styles.instructions}>Tamanho da grade:
                   {params.getRowsAmount()}x{params.getRowsAmount()}
           </Text>
-          <Field></Field>
-          <Field opened></Field>
-          <Field opened nearMines={1}></Field>
-          <Field opened nearMines={2}></Field>
-          <Field opened nearMines={3}></Field>
-          <Field opened nearMines={6}></Field>
-          <Field mined></Field>
-          <Field mined opened></Field>
-          <Field mined opened exploded></Field>
-          <Field flagged></Field>
-          <Field flagged opened></Field>
-
+          <View style={styles.board}>
+            <MineField board={this.state.board}></MineField>
+          </View>
         </SafeAreaView>
       </>
     );
@@ -42,10 +54,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-  }
+
 });
 
-export default App;
